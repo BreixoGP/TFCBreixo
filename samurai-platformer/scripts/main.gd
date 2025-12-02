07e36e59a31308bd66039c584ca611906f965080
Breixo Gonzalez <bgonzalezp24@fpcoruna.afundacion.org>
2025-12-02 09:05:17 +0100

    conectado boton de play con main scene, si le das a play comienza un neuvo juego

--- samurai-platformer/scripts/main.gd
+++ samurai-platformer/scripts/main.gd
@@ -2,20 +2,9 @@ extends Node2D
 @onready var jahmurai: Jahmurai = $Jahmurai
 @onready var levelcontainer: Node2D = $levelcontainer
 
-#var current_level: Node = null
 
 func _ready():
 	GameManager.player = jahmurai
 	GameManager.levelcontainer=levelcontainer
 	GameManager.load_current_level()
 	
-#func load_level(path: String):
-	#if current_level:
-		#current_level.queue_free()
-	
-	#var level_scene = load(path)
-	#current_level = level_scene.instantiate()
-	
-	#levelcontainer.add_child(current_level)
-	#var spawn=current_level.get_node("Spawn")
-	#jahmurai.global_position = spawn.global_position
