0020ec0b64bcc778e824bad14ff1c0958ce792aa
Breixo Gonzalez <bgonzalezp24@fpcoruna.afundacion.org>
2025-12-03 08:38:40 +0100

    creada deadzone en camara para movimiento ma suave

--- samurai-platformer/scripts/camera.gd
+++ samurai-platformer/scripts/camera.gd
@@ -13,20 +13,15 @@ func _ready() -> void:
 	target=GameManager.player
 	cam_float_pos=global_position
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if not enabled:
 		return
 	if target == null:
 		target=GameManager.player
 		return
-	
-	#follow_with_deadzone(delta)
+	follow_with_deadzone(_delta)
 	#apply_shake(delta)
-	var target_pos = target.global_position
-	cam_float_pos = cam_float_pos.lerp(target_pos, smooth)
-	cam_float_pos = _apply_limits(cam_float_pos)
-	global_position = cam_float_pos
-func follow_with_deadzone(delta):
+func follow_with_deadzone(_delta):
 	var cam_pos=global_position
 	var player_pos=target.global_position
 	
@@ -36,7 +31,6 @@ func follow_with_deadzone(delta):
 	var max_y = cam_pos.y + deadzone_size.y
 	
 	var new_pos=cam_pos
-	
 	#horizontal
 	if player_pos.x < min_x:
 		new_pos.x = player_pos.x + deadzone_size.x
@@ -48,18 +42,10 @@ func follow_with_deadzone(delta):
 	if player_pos.y > max_y:
 		new_pos.y = player_pos.y - deadzone_size.y
 		
-	#suavizado
-	
-	#global_position = global_position.lerp(new_pos, smooth)
-	#global_position = _apply_limits(global_position)
-	   # Suavizado
+	# suavizado
 	cam_float_pos = cam_float_pos.lerp(new_pos, smooth)
 	cam_float_pos = _apply_limits(cam_float_pos)
 
-	# Asignar posición redondeada al render
-	#var shaken_pos = cam_float_pos + shake_offset
-	#shaken_pos = _apply_limits(shaken_pos)
-	#global_position = shaken_pos.round()
 	global_position = cam_float_pos
 #func apply_shake(delta):
 	#if shake_strength > 0:
