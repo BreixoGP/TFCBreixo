18ab715c3847405faa02f95c408d5517f979abc2
Breixo Gonzalez <bgonzalezp24@fpcoruna.afundacion.org>
2026-01-13 18:15:22 +0100

    creado pick up key

--- samurai-platformer/scripts/key.gd
+++ samurai-platformer/scripts/key.gd
@@ -14,8 +14,8 @@ func _ready():
 func _on_body_entered(body):
 	if body is DrunkMaster:
 		#emit_signal("picked_up")
-		GameManager.has_crystal = true
-		GameManager.hud.update_crystal()
+		GameManager.has_key = true
+		GameManager.hud.update_items()
 		GameManager.collect_pickup(pickup_id)
 
 		queue_free()
